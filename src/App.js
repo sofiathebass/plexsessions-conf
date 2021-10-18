2da4c69bef3e031d149d5c3e34b865425a6aaba4
Sofia Theodoras <sofiathebass@berkeley.edu>
2021-10-18 13:28:43 -0700

    does this work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './components/Header'
+import Button from './components/Button'
+import Tabs from "./components/Tabs";
+
+const list = [1, 2, 3, 4];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title = 'Plextech Confessions'/>
+      <Tabs>
+        <div label="Confessions">
+          {list.map((item) => {
+            return (<h1>{item}</h1>)
+          })}
+        </div>
+        <div label="Login Page">
+          Get from Backend
+          <h3>Login Page for Admin Access</h3>
+          <button>Enter</button>
+          <h3>New Users Register Here</h3>
+        </div>
+        <div label="Post">
+        <h3>Write your Post</h3>
+        </div>
+        <div label="Admin">
+          Add to Backend
+        </div>
+      </Tabs>
     </div>
   );
 }
+
 
 export default App;
